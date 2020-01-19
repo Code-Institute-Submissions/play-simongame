e020318402a26a0902afe894c6374eeab970b6e2
daat2 <ibi.olu@gmail.com>
2020-01-19 22:51:05 +0000

    fix game button filter rule

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 // create game object with key and values
 
 var game={ //game object
+
+    UserSequence: [], //array containing the generated/random button
+    playSequence: [], //array containing the users selected button 
 	level: 0, //current level
 	turn: 0, //current turn
 	strict: 1, // user difficulty
@@ -10,7 +13,5 @@ var game={ //game object
 	active: false, //whether a turn is active or not
 	handler: false, // whether the click and sound handlers are active
 	shape: '.shape', // cached string for the button class
-	UserSequence: [], //array containing the generated/random button
-    playSequence: [], //array containing the users selected button 
     let win ; //winner sound display 
     let flash; //this will flash the button wich trigger the sound 
