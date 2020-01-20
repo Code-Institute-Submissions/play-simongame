2c73842eaf35568668ee63bbd74b252c9938b156
daat2 <ibi.olu@gmail.com>
2020-01-20 16:50:28 +0000

    add variables for game

--- js/main.js
+++ js/main.js
@@ -2,48 +2,29 @@
 
 // create game object with key and values
 
-var game={ //game object
+//variabes for game  
 
-    UserSequence: [], //array containing the generated/random button
-    playSequence: [], //array containing the users selected button 
-	level: 0, //current level
-	turn: 0, //current turn
-	strict: 1, // user difficulty
-    score: 0, //current score
-	active: false, //whether a turn is active or not
-	handler: false, // whether the click and sound handlers are active
-<<<<<<< HEAD
-	shape: shape, // cached string for the button class
-}
-var boardSound =[
+    UserSequence= []; //array containing the generated/random button
+    playSequence= []; //array containing the users selected button 
+    const Numlevels =20;
+    var id, color,level =0; //current level
+    var strict  =false;
+    var error =false;
+    	turn= 0, //current turn
+	strict= 1, // user difficulty
+    score= 0, //current score
+	active= false, //whether a turn is active or not
+	handler= false, // whether the click and sound handlers are active
+	shape= shape, // cached string for the button class
 
-			
-    "https://freesound.org/data/previews/337/337112_5872390-lq.mp3"	 //blue
-    "https://freesound.org/data/previews/128/128229_2238068-lq.mp3"	//green
+ var boardSound =[
+
+    "https://freesound.org/data/previews/337/337112_5872390-lq.mp3"	, //blue
+    "https://freesound.org/data/previews/128/128229_2238068-lq.mp3",	//green
 	
-        "https://freesound.org/data/previews/151/151022_1838182-lq.mp3" //oranges
+        "https://freesound.org/data/previews/151/151022_1838182-lq.mp3", //oranges
         
-        "https://freesound.org/data/previews/128/128349_2238068-lq.mp3" //yellow
-        "https://freesound.org/data/previews/151/151605_57789-lq.mp3"//winner
+        "https://freesound.org/data/previews/128/128349_2238068-lq.mp3", //yellow
+        "https://freesound.org/data/previews/151/151605_57789-lq.mp3",//winner
 	
 ];
-=======
-    shape: shape, // cached string for the button class
-    flash :'function', // to make the button flash
-}
-
-//start game sequence 
-$(document).ready(function() {
-    $("start").click(function() {
-        level++;
-        startSequence();
-    })
-})
-
-//playSequence
-functon startSequence() {
-    $(".display").text(level);
-}
-
-
->>>>>>> 48811b68f4ea6d372b2a354ab98282293e078888
