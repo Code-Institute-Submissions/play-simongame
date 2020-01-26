0e39b615df92f0672a79e05a1751894363895396
daat2 <ibi.olu@gmail.com>
2020-01-26 20:52:50 +0000

    fix button presses

--- js/main.js
+++ js/main.js
@@ -1,47 +1,93 @@
-//variables 
 
 // create game object with key and values
 
-//variabes for game  
 
-    UserSequence= []; //array containing the generated/random button
-    playSequence= []; //array containing the users selected button 
-    const Numlevels =20;
-    var id, color,level =0; //current level
-    var strict  =false;
-    var error =false;
-    	turn= 0, //current turn
-	strict= 1, // user difficulty
-    score= 0, //current score
-	active= false, //whether a turn is active or not
-	handler= false, // whether the click and sound handlers are active
-	shape= shape, // cached string for the button class
-var boardSound [
-    "https://freesound.org/data/previews/337/337112_5872390-lq.mp3"	, //blue
-    "https://freesound.org/data/previews/128/128229_2238068-lq.mp3",	//green
-	
-        "https://freesound.org/data/previews/151/151022_1838182-lq.mp3", //oranges
-        
-        "https://freesound.org/data/previews/128/128349_2238068-lq.mp3", //yellow
-        "https://freesound.org/data/previews/151/151605_57789-lq.mp3",//winner
-	
+
+
+//variabes for game  
+let game= {
+    UserSequence= [], //array containing the generated/random button
+    playSequence= [], //array containing the users selected button 
+    Numlevels =20,
+    flash= [],//no of button flashes 
+    level :1 ,//current level
+    turn : 0, //current turn
+	strict :false, // user difficulty mode.
+    score: 0, //current score
+	active: false, //whether a turn is active or not
+	handler: false, // whether the click and sound handlers are active
+	shape: 'shape', //  string for the color button class
+
 };
 
- //start board sequence 
- $(document). ready(function(){
-$(".score").text("");
+
+const newLocal = newFunction();
+let boardSound =[
+    newLocal,						
+    
+  "https://freesound.org/data/previews/337/337112_5872390-lq.mp3",  //blue button
+  "https://freesound.org/data/previews/128/128229_2238068-lq.mp3" ,	 // green button					
+   
+
+ "https://freesound.org/data/previews/151/151022_1838182-lq.mp3" , //orange button 						
+
+ "https://freesound.org/data/previews/128/128349_2238068-lq.mp3", //yellow	button					
 
 
- })
-$("start").click(function(){
-strict =false;
-error =false;
-level=0;
-level++;
-playSequence=[];
-UserSequence=[];
-strict=true;
-playSequence();
+"https://freesound.org/data/previews/151/151605_57789-lq.mp3",// winner button			 
 
 
-})
+];
+
+  
+
+const blue = document.querySelector('shapeB');
+const yellow = document.querySelector('shapeY ');
+const green = document.querySelector('shapeG');
+const orange = document.querySelector('shapeO');
+
+button.addEventListener('click', (event) =>{
+
+    $(".score").text("")
+    $("start") .addEventListener('click', event => {
+        
+    }
+       strict = false;
+    const newLocal = level = 0;
+        level ++;
+        simonSeq=[];
+        userSeq=[];
+        SimonSequence();
+}
+
+function newFunction() {
+    return "https://freesound.org/data/previews/337/337112_5872390-lq.mp3";
+}
+
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 4)
+}
+function deriveButton(number){
+    switch(number){
+        case 1:
+            return (green, greenSound, "")
+    }
+}
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 4)
+}
+function deriveButton(number){
+    switch(number){
+        case 1:
+            return (green, greenSound, "lightgreen")
+    }
+}
+function playSequence(number){
+    color, sound, colorName = deriveButton(number)
+    color.changebackground(colorName)
+    sound.play()
+}
+for (number in playSequence){
+    userSequence(number)
+}  
+
