2e2470a4129fbf4fdf4b1d40ef4cfec4f52866e6
daat2 <ibi.olu@gmail.com>
2020-01-28 12:28:56 +0000

    fix button sound

--- js/main.js
+++ js/main.js
@@ -1,93 +1,45 @@
-
-// create game object with key and values
-
-
-
-
-//variabes for game  
-let game= {
-    UserSequence= [], //array containing the generated/random button
-    playSequence= [], //array containing the users selected button 
-    Numlevels =20,
-    flash= [],//no of button flashes 
-    level :1 ,//current level
-    turn : 0, //current turn
-	strict :false, // user difficulty mode.
-    score: 0, //current score
-	active: false, //whether a turn is active or not
-	handler: false, // whether the click and sound handlers are active
-	shape: 'shape', //  string for the color button class
-
+const game = {
+    userSequence: [], //array containing the generated/random button
+    playSequence: [], //array containing the users selected button
+    numLevels: 20
 };
-
-
-const newLocal = newFunction();
-let boardSound =[
-    newLocal,						
-    
-  "https://freesound.org/data/previews/337/337112_5872390-lq.mp3",  //blue button
-  "https://freesound.org/data/previews/128/128229_2238068-lq.mp3" ,	 // green button					
-   
-
- "https://freesound.org/data/previews/151/151022_1838182-lq.mp3" , //orange button 						
-
- "https://freesound.org/data/previews/128/128349_2238068-lq.mp3", //yellow	button					
-
-
-"https://freesound.org/data/previews/151/151605_57789-lq.mp3",// winner button			 
-
-
+const boardSound = [
+    "https://freesound.org/data/previews/337/337112_5872390-lq.mp3",    // blue button
+    "https://freesound.org/data/previews/128/128229_2238068-lq.mp3",	// green button
+    "https://freesound.org/data/previews/151/151022_1838182-lq.mp3",   // orange button
+    "https://freesound.org/data/previews/128/128349_2238068-lq.mp3",    // yellow button
+    "https://freesound.org/data/previews/151/151605_57789-lq.mp3"      // winner button
 ];
-
-  
-
-const blue = document.querySelector('shapeB');
-const yellow = document.querySelector('shapeY ');
-const green = document.querySelector('shapeG');
-const orange = document.querySelector('shapeO');
-
-button.addEventListener('click', (event) =>{
-
-    $(".score").text("")
-    $("start") .addEventListener('click', event => {
-        
-    }
-       strict = false;
-    const newLocal = level = 0;
-        level ++;
-        simonSeq=[];
-        userSeq=[];
-        SimonSequence();
-}
-
-function newFunction() {
-    return "https://freesound.org/data/previews/337/337112_5872390-lq.mp3";
-}
-
+const blue = document.querySelector('#shapeB');
+const green = document.querySelector('#shapeY');
+const orange = document.querySelector('#shapeG');
+const yellow = document.querySelector('#shapeO');
+const start = document.querySelector('.start');
+start.addEventListener('click', (event) => {
+    $(".count").text("01");
+    let number = generateRandomNumber()
+    game.playSequence.push(number)
+    play(game.playSequence)
+});
 function generateRandomNumber(){
     return Math.floor(Math.random() * 4)
 }
-function deriveButton(number){
+function derive(number){
     switch(number){
+        case 0:
+            return [blue, boardSound[0], "blue-active"]
         case 1:
-            return (green, greenSound, "")
+            return [green, boardSound[1], "green-active"]
+        case 2:
+            return [orange, boardSound[2], "orange-active"]
+        case 3:
+            return [yellow, boardSound[3], "yellow-active"]
     }
 }
-function generateRandomNumber(){
-    return Math.floor(Math.random() * 4)
+function play(sequence){
+    sequence.forEach(function (number) {
+        let [button, sound, className] = derive(number)
+        button.classList.add(className)
+        new Audio(sound).play()
+    })
 }
-function deriveButton(number){
-    switch(number){
-        case 1:
-            return (green, greenSound, "lightgreen")
-    }
-}
-function playSequence(number){
-    color, sound, colorName = deriveButton(number)
-    color.changebackground(colorName)
-    sound.play()
-}
-for (number in playSequence){
-    userSequence(number)
-}  
-
