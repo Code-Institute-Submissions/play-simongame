38531f437eeece00cc3ab077de5448308d74baf5
daat2 <ibi.olu@gmail.com>
2020-02-20 17:07:00 +0000

    fix board console

--- js/main.js
+++ js/main.js
@@ -11,95 +11,127 @@ const boardSound = [
     "https://freesound.org/data/previews/191/191591_2437358-lq.mp33",    // orange button
     "https://freesound.org/data/previews/151/151605_57789-lq.mp3",    // winner button
 ];
-const blue = document.querySelector('#shapeB');
-const green = document.querySelector('#shapeY');
-const orange = document.querySelector('#shapeG');
-const yellow = document.querySelector('#shapeO');
-const start = document.querySelector('.start');
-const counter = document.querySelector('.counter');
-let level = 0;
 
-document.getElementById("strict").addEventListener("click", function () {
-    console.log("checked")
-});
+let green = document.getElementById("shapeG")
+let blue = document.getElementById("shapeB")
+let orange = document.getElementById("shapeO")
+let yellow = document.getElementById("shapeY")
 
+let strict=false;
+let levelCount=1;
+let noise =true;
+let win;
+
+let on =false;
 
 //Start board console 
-$(document).ready(function () {
+//Power button
+
+if (powerSwitch.addEventListener('click' , (event) =>{
+    if (power =="off")
+    
+
+        $("strictButton".addclass("fa fa-check"));
+        strict="on";
+    }
+    });
+
+
+
+
+
+
+//To turn the button On 
+onButton.addEventListener('click' , (event)=>{
+    if (buttonOn.checked ==true){
+        strict=true;
+    }else{
+        strict =false;
+
+    }
+});
+//start 
+strictButton.addEventListener('click' , (event)=>{
+    if (strictButton.checked =true){
+        strict=true;
+    }else{
+        strict =false;
+
+        $("strictButton".addclass("fa fa-check"));
+        strict="on";
+    }
+    });
+
+
+
+
+
+
+
+
 
     
 
-    $(".start").click(function () {
-        level = level + 1;
-        $(counter).text("01");
-        // playerSequence = [];
-        // simonSequence = [];
-        // simonSequence();
-        // let number = generateRandomNumber()
-        // game.simonSequence.push(number)
-        // play(game.simonSequence)
 
+//     function generateRandomNumber() {
+//         return Math.floor(Math.random() * 4)
 
-    });
-    function generateRandomNumber() {
-        return Math.floor(Math.random() * 4)
-
-        function derive(number) {
-            switch (number) {
-                case 0:
-                    return [blue, boardSound[0], "blue-active"]
-                case 1:
-                    return [green, boardSound[1], "green-active"]
-                case 2:
-                    return [orange, boardSound[2], "orange-active"]
-                case 3:
-                    return [yellow, boardSound[3], "yellow-active"]
-            }
-        }
-    }
-});
-
-//     start.addEventListener('click', (event) => {
-//       $(".counter").text("01");
-//         let number = generateRandomNumber()
-//     game.simonSequence.push(number)
-//     play(game.simonSequence)
+//         function derive(number) {
+//             switch (number) {
+//                 case 0:
+//                     return [blue, boardSound[0], "blue-active"]
+//                 case 1:
+//                     return [green, boardSound[1], "green-active"]
+//                 case 2:
+//                     return [orange, boardSound[2], "orange-active"]
+//                 case 3:
+//                     return [yellow, boardSound[3], "yellow-active"]
+//             }
+//         }
+//     }
 // });
-// function generateRandomNumber(){
-//     return Math.floor(Math.random() * 4)
-// }
-// function derive(number){
-//     switch(number){
-//         case 0:
-//             return [blue, boardSound[0], "blue-active"]
-//         case 1:
-//             return [green, boardSound[1], "green-active"]
-//         case 2:
-//             return [orange, boardSound[2], "orange-active"]
-//         case 3:
-//             return [yellow, boardSound[3], "yellow-active"]
-//     }
-// }
-// function play(sequence){
-//     sequence.forEach(function (number) {
-//         let [button, sound, className] = derive(number)
-//         button.classList.add(className)
-//         new Audio(sound).play()
-//     })
-// }
-// function play(){
-//     playerSequence =[];
-//     simonSequence=[0,1,2,];
-//     flash=0;
-//     for (var i =0; i <20; i++){
-//         simonSequence.push(Math.floor(Math.random()*4 )+1);
-//         console.log(simonSequence);
-//     }
-//     simonSequence =true;
-//     intervalId = setInterval(counter,1000 ); {
-//     }
-// }
-// function playSound (id){
-//     var sound = new Audio (boardSound[id]);
-//                sound.play();
-// }
+
+// //     start.addEventListener('click', (event) => {
+// //       $(".counter").text("01");
+// //         let number = generateRandomNumber()
+// //     game.simonSequence.push(number)
+// //     play(game.simonSequence)
+// // });
+// // function generateRandomNumber(){
+// //     return Math.floor(Math.random() * 4)
+// // }
+// // function derive(number){
+// //     switch(number){
+// //         case 0:
+// //             return [blue, boardSound[0], "blue-active"]
+// //         case 1:
+// //             return [green, boardSound[1], "green-active"]
+// //         case 2:
+// //             return [orange, boardSound[2], "orange-active"]
+// //         case 3:
+// //             return [yellow, boardSound[3], "yellow-active"]
+// //     }
+// // }
+// // function play(sequence){
+// //     sequence.forEach(function (number) {
+// //         let [button, sound, className] = derive(number)
+// //         button.classList.add(className)
+// //         new Audio(sound).play()
+// //     })
+// // }
+// // function play(){
+// //     playerSequence =[];
+// //     simonSequence=[0,1,2,];
+// //     flash=0;
+// //     for (var i =0; i <20; i++){
+// //         simonSequence.push(Math.floor(Math.random()*4 )+1);
+// //         console.log(simonSequence);
+// //     }
+// //     simonSequence =true;
+// //     intervalId = setInterval(counter,1000 ); {
+// //     }
+// // }
+// // function playSound (id){
+// //     var sound = new Audio (boardSound[id]);
+// //                sound.play();
+// // }
