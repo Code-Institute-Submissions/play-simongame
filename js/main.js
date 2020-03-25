b506ad5062763946a07d6e00cfadd6c14fc53681
daat2 <ibi.olu@gmail.com>
2020-03-25 12:57:49 +0000

    testing sound button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const game = {
-    playerSequence: [], //array containing the generated/random button
-    simonSequence: [], //array containing the users selected button
+    playerSequence: [], //array containing the users selection
+    simonSequence: [], //array containing generated random buttons
     numLevels: 20,
     turn: 00, //
     active: false,
@@ -27,7 +27,6 @@ let strictButton = document.getElementById("strict");
 let startButton = document.getElementById("start");
 
 
-
 let power;
 let off;
 let strict = false;
@@ -35,11 +34,11 @@ let on;
 
 
 
-// let blueSound = new Audio( "https://freesound.org/data/previews/151/151022_1838182-lq.mp3")
-//  let yellowSound = new Audio ("https://freesound.org/data/previews/156/156859_2538033-lq.mp3")  
-// let greenSound = new Audio ("https://freesound.org/data/previews/171/171495_2437358-lq.mp3" )
-// let orangeSound = new Audio ("https://freesound.org/data/previews/191/191591_2437358-lq.mp33")
-// let winSound = new Audio ( "https://freesound.org/data/previews/151/151605_57789-lq.mp3")
+let blueSound = new Audio( "https://freesound.org/data/previews/151/151022_1838182-lq.mp3")
+ let yellowSound = new Audio ("https://freesound.org/data/previews/156/156859_2538033-lq.mp3")  
+let greenSound = new Audio ("https://freesound.org/data/previews/171/171495_2437358-lq.mp3" )
+let orangeSound = new Audio ("https://freesound.org/data/previews/191/191591_2437358-lq.mp33")
+let winSound = new Audio ( "https://freesound.org/data/previews/151/151605_57789-lq.mp3")
 
 
 $(".boardSwitch").on("click", function () {
@@ -76,3 +75,32 @@ startButton.addEventListener('click', (event) => {
   
   }
 });
+
+start.addEventListener('click', (event) => {
+    $(".displayText").text("01");
+    let number = generateRandomNumber()
+    game.playerSequence.push(number)
+    play(game.playerSequence)
+});
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 4)
+}
+function derive(number){
+    switch(number){
+        case 0:
+            return [blue, boardSound[0], "blue-active"]
+        case 1:
+            return [green, boardSound[1], "green-active"]
+        case 2:
+            return [orange, boardSound[2], "orange-active"]
+        case 3:
+            return [yellow, boardSound[3], "yellow-active"]
+    }
+}
+function play(sequence){
+    sequence.forEach(function (number) {
+        let [button, sound, className] = derive(number)
+        button.classList.add(className)
+        new Audio(sound).play()
+    })
+}
