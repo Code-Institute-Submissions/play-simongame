980dea18cecd1c657a8d61bbaee9ea7ce77921ed
daat2 <ibi.olu@gmail.com>
2020-04-07 23:29:41 +0000

    updated audio file name

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ const boardSound = [
     "https://freesound.org/data/previews/151/151022_1838182-lq.mp3",    // blue button
     "https://freesound.org/data/previews/156/156859_2538033-lq.mp3",	// yellow button
     "https://freesound.org/data/previews/171/171495_2437358-lq.mp3",   // green button
-    "https://freesound.org/data/previews/191/191591_2437358-lq.mp33",    // orange button
+    "https://freesound.org/data/previews/191/191591_2437358-lq.mp3",    // orange button
     "https://freesound.org/data/previews/151/151605_57789-lq.mp3",    // winner button
 ];
 
@@ -37,33 +37,34 @@ let on;
 let blueSound = new Audio( "https://freesound.org/data/previews/151/151022_1838182-lq.mp3")
  let yellowSound = new Audio ("https://freesound.org/data/previews/156/156859_2538033-lq.mp3")  
 let greenSound = new Audio ("https://freesound.org/data/previews/171/171495_2437358-lq.mp3" )
-let orangeSound = new Audio ("https://freesound.org/data/previews/191/191591_2437358-lq.mp33")
+let orangeSound = new Audio ("https://freesound.org/data/previews/191/191591_2437358-lq.mp3")
 let winSound = new Audio ( "https://freesound.org/data/previews/151/151605_57789-lq.mp3")
 
 
 $(".boardSwitch").on("click", function () {
-    if (power == off) {
+    if (power == false) {
         $("#displayText").css("opacity", "0.1");
-        power = on;
+        power = true;
     }
-    else if (power == on) {
+    else if (power == true) {
         displayText.innerHTML = "--";
-        power = off;
+        power = false;
+        strict= false;
     };
 
 });
 strictButton.addEventListener('click', (event) => {
-    if (power == on) {
+    if (power == true) {
         if (strict = false) {
-            $("#strictButton").css("background", "coral");
+            // $("#strictButton").css("background", "coral");
             $("#strictButton").addClass("fa fa-check");
-            strict = "on";
+            strict = "true";
         }
-        else if (power == off) {
+        else if (power == false) {
             (strict == true)
             $("#strictButton").css("background", "violet");
             $("#strictButton").removeClass("fa fa-check");
-            strict = off;
+            strict = false;
         };
     };
 });
@@ -75,33 +76,4 @@ startButton.addEventListener('click', (event) => {
   
   }
 });
-
-start.addEventListener('click', (event) => {
-    $(".displayText").text("01");
-    let number = generateRandomNumber()
-    game.playerSequence.push(number)
-    play(game.playerSequence)
-});
-function generateRandomNumber(){
-    return Math.floor(Math.random() * 4)
-}
-function derive(number){
-    switch(number){
-        case 0:
-            return [blue, boardSound[0], "blue-active"]
-        case 1:
-            return [green, boardSound[1], "green-active"]
-        case 2:
-            return [orange, boardSound[2], "orange-active"]
-        case 3:
-            return [yellow, boardSound[3], "yellow-active"]
-    }
-}
-function play(sequence){
-    sequence.forEach(function (number) {
-        let [button, sound, className] = derive(number)
-                new Audio(sound).play()
-    })
-};
-
 
