6672caf7864fba9d00793ee2ccb12c7fd4831bb1
daat2 <ibi.olu@gmail.com>
2020-04-30 13:00:28 +0000

    code review on button

--- js/main.js
+++ js/main.js
@@ -20,13 +20,12 @@ let green = document.getElementById("shapeG");
 let blue = document.getElementById("shapeB");
 let orange = document.getElementById("shapeO");
 let yellow = document.getElementById("shapeY");
-//  let onButton =  document.getElementById("#onoffSwitch");
+ let onButton =  document.getElementById("#onoffSwitch");
 // let displayCount = document.getElementById("displayText");
 // let boardSwitch = document.querySelector(".boardSwitch");
 let strictButton = document.getElementById("strict");
 let startButton = document.getElementById("start");
 let displayCountMemory = document.getElementById("displayCount");
-
 
 
 
@@ -54,7 +53,7 @@ let winSound = new Audio ( "https://freesound.org/data/previews/151/151605_57789
 
 $("#onoffSwitch").on("click", function () {
     if (power == false) {
-          $("#displayText").text("@")
+          $("#displayText").text("on")
         power = true;
     }
     else if(power == true){
@@ -68,7 +67,6 @@ $("#onoffSwitch").on("click", function () {
    }
 
 
-
 //StrictButton  Function
 
 $("#strictButton").on("click",function(){
@@ -78,75 +76,12 @@ $("#strictButton").on("click",function(){
    $("#strictButton").addClass("fa fa-check");
   strict = "true";
 
-        }
-    else if (strict = true){
+        };
+    }
+    else if (strict == true){
     $("#strictButton").css("background", "navy");
    $("#strictButton").removeClass("fa fa-check");
   strict = "false";
-    }
-}
-});
-//Start button 
-
-start.addEventListener('click',(event)=>{
-if(on|| win)
-playSound();
-});
-
-function playSound(){
-flashTracker=[];
-playerSequence=[];
-flash=0;
-displayCount= 1;
- displayCountMemory.innerHTML =1;
-gameStatus=true;
-for( var i =0; i < 20; i++){
-    playerSequence.push(Math.floor(Math.random() *4) +1);
-
-}
-
-simonSequence =true;
-
-var myInterval =setInterval(function(){
-   myInterval =setInterval(gameTurn,1000);
-
-
-   function gameTurn() {
-       on =false;
-
-       if (flash == turn ){
-           clearInterval(myInterval);
-           simonSequence = false;
-           clearColor();
-           on =true;
-       }
-       if (simonSequence){
-           clearColor();
-           setTimeout(()=>{
-
-            if(flashTracker[flash]== 0)blue();
-             if(flashTracker[flash]== 1)green();
-             if(flashTracker[flash]== 2)orange();
-             if(flashTracker[flash]==3) yellow();
-               flash++;
-            }, 50);
-
-           function derive(number){
-    switch(number){
-        case 0:
-            return [blue, boardSound[0], "blue-active"]
-        case 1:
-            return [green, boardSound[1], "green-active"]
-        case 2:
-            return [orange, boardSound[2], "orange-active"]
-        case 3:
-            return [yellow, boardSound[3], "yellow-active"]
     };
-};
-function play(sequence){
-    sequence.forEach(function (number) {
-        let [button, sound, className] = derive(number)
-        button.classList.add(className)
-        new Audio(sound).play();
-    }  
-};
+})
+})
