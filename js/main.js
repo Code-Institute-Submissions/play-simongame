ac1a35993df3932e66a72fd56c9f42661d924ad5
daat2 <ibi.olu@gmail.com>
2020-06-16 21:21:50 +0000

    fix start button issue

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@
 //     "https://freesound.org/data/previews/151/151605_57789-lq.mp3",    // winner button
 // ];
 
+
 let green = document.getElementById("shapeG");
 let blue = document.getElementById("shapeB");
 let orange = document.getElementById("shapeO");
@@ -28,7 +29,7 @@ let displayCountMemory = document.getElementById("displayCount");
 
 //Variables
 
-let power= false;
+let power = false;
 let playerSequence = [];
 let genSequence=[];
 let displayCount = 1;
@@ -39,6 +40,7 @@ let intervalId = 0;
 let clearIntervalId;
 let noise;
 let randNum;
+let level = 1;
 // let activeRunning = false;
 // let activeMemory;
 // let trackCount; //keep track of turn
@@ -63,20 +65,13 @@ let winSound = new Audio("https://freesound.org/data/previews/151/151605_57789-l
 
 $("#onoffSwitch").click(function () {
     if (power == false) {
-        $("#displayText").text("ON");
+        $("#displayCount").text("ON");
         power = true;
-        console.log(power);
-     
-    }
-    else {
-
-        $("#displayText").text("OFF");
+        console.log('Power', power);
+    } else {
+        $("#displayCount").text("OFF");
         power = false;
-        strict = false;
-        console.log(power);
-
-
-
+        console.log('Power', power);
     }
 });
 
@@ -106,37 +101,45 @@ $("#strictButton").click(function () {
 
 //Start Button Functionality //
 
- $("#startButton").click(function () {
-        if (power || win) {
-            play(sound);
-            
-        };
-    });
+$("#startButton").click(function () {
+    console.log('Start playing');
+    genSequence = [];
+    playerSequence = [];
+    flash = 0;
+    intervalId = 0;
+    win = false;
+    displayCount = 1;
+    displayCountMemory.innerHTML = displayCount;
+    positive = true;
+    generateRandomNum();
+});
+
+// Flash light and play sound of the first 
 
 
 
-    //  Start of th game //
+//  Start of the game //
+//function to start the game
+// function play(){
+//     console.log('Start playing');
+//     genSequence = [];
+//     playerSequence = [];
+//     flash = 0;
+//     intervalId = 0;
+//     win = false;
+//     displayCount = 1;
+//     displayCountMemory.innerHTML = 1;
+//     positive = true;
+//     generateRandomNum();
+// }
 
-     //function to start the game
-    function play(sound) {
-     genSequence = [];
-      playerSequence=[];
-        flash = 0;
-        intervalId = 0;
-        win = false;
-        displayCount= 1;
-        displayCountMemory.innerHTML = 1;
-        positive = true;
-        generateRandomNum();
-
-    function generateRandomNum(){
-           for (var i = 0; i < numLevels; i++) {
-               let randNum = (Math.floor(Math.random() * 4) + 1);
-    genSequence.push(randNum);
-    console.log(genSequence);
-        }
+function generateRandomNum(){
+    for (var i = 0; i < numLevels; i++) {
+        let randNum = (Math.floor(Math.random() * 4) + 1);
+        genSequence.push(randNum);
     }
-    }
+    console.log('Gen Sequence', genSequence);
+}
 
     //flashing light  //
 
@@ -149,113 +152,110 @@ function gameCount(){
     clearColor(button);
     
 
-if (simonCount){
-    power= false;
-    clearColor();
-    setTimeOut(function(){
-        flashColor(genSequence[flash])
-        flash++;
-    },300);
+    if (simonCount){
+        power = false;
+        setTimeOut(function(){
+            flashColor(genSequence[flash])
+            flash++;
+        },300);
+        clearColor();
     }
 };
 
 function flashColor(id){
+    if (id == 1){
         shapeB.style.backgroundColor = "lightblue" ;
+    } else if (id == 2) {
         shapeY.style.backgroundColor = "lightyellow";
-        shapeG.style.backgroundColor = "SpringGreen" ;//#00FF7F
-        shapeO.style.backgroundColor = "OrangeRed" ;//#FF4500
+    }
+    
+    
+    shapeG.style.backgroundColor = "SpringGreen" ;//#00FF7F
+    shapeO.style.backgroundColor = "OrangeRed" ;//#FF4500
 
 
-    };
+};
+
 function clearColor(){
      shapeB.style.backgroundColor = "blue";
      shapeY.style.backgroundColor = "yellow";
      shapeG.style.backgroundColor = "green";
      shapeO.style.backgroundColor = "orange";
-
 };
 
 // Sounds for button clicks //
  
 
 $("#shapeB").click(function(){
-addpplayerSequence(0);
-
+    addpplayerSequence(0);
 });
-$("#shapeY").click(function(){
-addpplayerSequence(1);
 
+$("#shapeY").click(function(){
+    addpplayerSequence(1);
 });
 
 $("#shapeG").click(function(){
-addpplayerSequence(2);
-
+    addpplayerSequence(2);
 });
 
 $("#shapeO").click(function(){
-addpplayerSequence(3);
-
+    addpplayerSequence(3);
 });
 
 
 
  function addpplayerSequence(id){
-     if(power){
-   playerSequence.push(id);
-   checkSequence();
-   flashColor();
-           if (!win) {
-            setTimeout(() => {
-           clearColor();
-            }, 300);
-           
-        }
-
-     }
+    if(power){
+        playerSequence.push(id);
+        checkSequence();
+        flashColor();
+            if (!win) {
+                setTimeout(() => {
+                    clearColor();
+                }, 300);
+            }
+    }
 };
 
 
 function play(sound){
   
-if (noise) {
+    if (noise) {
         let audio = document.getElementById("soundB");
-       audio.play(sound);
+        audio.play(sound);
     }
-         noise = true;
+    noise = true;
     $("#shapeB").css ("background-color","red");
 }
 
-if (noise) {
-        let audio = document.getElementById("soundY");
-       audio.play(sound);
-    }
-         noise = true;
-    $("#shapeY").css ("background-color","red");
+// if (noise) {
+//         let audio = document.getElementById("soundY");
+//         audio.play(sound);
+// }
+// noise = true;
+// $("#shapeY").css ("background-color","red");
 
 
-    if (noise) {
-        let audio = document.getElementById("soundG");
-       audio.play(sound);
-    
-         noise = true;
-    $("#shapeG").css ("background-color","LawnGreen"); //#7CFC00
-    }
-if (noise) {
-        let audio = document.getElementById("soundO");
-       audio.play(sound);
-}
-         noise = true;
-    $("#shapeO").css ("background-color","Indigo"); //#4B0082
+// if (noise) {
+//     let audio = document.getElementById("soundG");
+//     audio.play(sound);
 
+//     noise = true;
+//     $("#shapeG").css ("background-color","LawnGreen"); //#7CFC00
+// }
+// if (noise) {
+//     let audio = document.getElementById("soundO");
+//     audio.play(sound);
+// }
+// noise = true;
+// $("#shapeO").css ("background-color","Indigo"); //#4B0082
 
 
 function checkSequence(){
     if (playerSequence[playerSequence.length - 1] !== genSequence[playerSequence.length - 1]){
-positive =false;
+        positive =false;
      };
-if (playerSequence.length == numLevels && positive){
-    WinGame();
-}
-
-
+    if (playerSequence.length == numLevels && positive){
+        WinGame();
+    }
 };
