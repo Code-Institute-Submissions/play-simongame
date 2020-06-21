bbebf62ee662ef099e6f5e8d29f2ff908dcf898b
daat2 <ibi.olu@gmail.com>
2020-06-21 16:22:01 +0000

    fix to the flash time out error

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//Variables
 let green = document.getElementById("shapeG");
 let blue = document.getElementById("shapeB");
 let orange = document.getElementById("shapeO");
@@ -7,36 +9,23 @@ let onoffButton = document.getElementById("#onoffSwitch");
 // let boardSwitch = document.querySelector(".boardSwitch");
 let strictButton = document.getElementById("strict");
 let startButton = document.getElementById("start");
-let displayCountMemory = document.getElementById("displayCount");
 
+/*************************/
 //Variables
-
-let power = false;
-let gameStarted = false;
+let displayCountMemory = document.getElementById("displayCount");
 let playerSequence = []; // user sequence
 let simonSeq = []; //computer sequence flash tracker//
-let genCompTurn; // computerturn or user turn (boolean)//
-let level = 0; //  keep track of turn//
- 
+let level = 0; //  keep track of turn
+let power = false;
+let gameStarted = false;
 let strict = false;
 let numLevels = 20; //Total number to declare a winner
-let simonCount; //computer turn
-let intervalId = 0;
-let clearIntervalId;
-let noise;
-let randNum;
-// let activeRunning = false;
-// let activeMemory;
-// let trackCount; //keep track of turn
-// let memoryArray=[];
-let flash;
+let flashes = 0
 let sound = true;
-let win = false;
-
-let positive; // To check players performance (true or false)
-// let newAudio;
-// let playGame; // start of the game 
 let onoffSwitch = false; //power button on or off function
+
+
+// Sound Variables //
 
 let blueSound = new Audio("https://freesound.org/data/previews/151/151022_1838182-lq.mp3")
 let yellowSound = new Audio("https://freesound.org/data/previews/156/156859_2538033-lq.mp3")
@@ -44,10 +33,14 @@ let greenSound = new Audio("https://freesound.org/data/previews/171/171495_24373
 let orangeSound = new Audio("https://freesound.org/data/previews/191/191591_2437358-lq.mp3")
 let winSound = new Audio("https://freesound.org/data/previews/151/151605_57789-lq.mp3")
 let clickSound = new Audio("https://freesound.org/data/previews/522/522720_10058132-lq.mp3")
+
+
+
+
 /* ****  *** ********** */
 
-// Power button to test the game button switch//
 
+// Power button to test the game button switch
 $("#onoffSwitch").click(function () {
     if (power == false) {
         $("#displayCount").text("ON");
@@ -63,11 +56,7 @@ $("#onoffSwitch").click(function () {
         console.log('Power', power);
     }
 });
-
-
-//Strict Button Functionality //
-
-
+//Strict Button Functionality
 $("#strictButton").click(function () {
     if (power == true) {
         if (strict == false) {
@@ -76,7 +65,6 @@ $("#strictButton").click(function () {
             strict = true;
             console.log(strict);
         }
-
         else if (strict == true) {
             $("#strictButton").css("background", "white");
             $("#strictButton").removeClass("fa fa-check");
@@ -86,191 +74,122 @@ $("#strictButton").click(function () {
         }
     }
 });
-
-//Start Button Functionality //
-
+//Start Button Functionality
 $("#startButton").click(function() {
     if(power && !gameStarted){
         startGame();
         gameStarted = true
     };
-
-    //Fuction for game start //
     function startGame(){
         console.log('Start Game');
-        level = 5;
+        level = 1;
         generateRandomNum();
         play(level)
     }
 });
-
 function generateRandomNum() {
-    for (var i = 0; i < numLevels; i++) {
+    for (let i = 0; i < numLevels; i++) {
         let randNum = (Math.floor(Math.random() * 4) + 1);
         simonSeq.push(randNum);
     }
     console.log('simon Sequence', simonSeq);
 }
-
 function play(stage){
     displayCountMemory.innerHTML = stage;
-    for(i = 0; i < stage; i++){
+    flashes = 0
+    for(let i = 0; i < stage; i++){
         playButton(i)
     }
 }
-
 function playButton(index){
     setTimeout(function(){
         console.log("Play " + simonSeq[index])
         playButtonPad(simonSeq[index])
-    }, 3000 * (index + 1))
+        flashes++
+    }, 2000 * (index + 1))
 }
-
-// Sound for simonSeq button//
-
+// Plays a button by Id
 function playButtonPad(id){
-    switch (id) {
-        case 0:
-            $("#shapeB").addClass('blue-active');
-            playSound(blueSound);
-            break;
-        case 1:
-            $("#shapeY").addClass('yellow-active');
-            playSound(yellowSound);
-            break;
-        case 2:
-            $("#shapeG").addClass('green-active');
-            playSound(greenSound);
-            break;
-        case 3:
-            $("#shapeO").addClass('orange-active');
-            playSound(orangeSound);
-            break;
+    if (power){
+        switch (id) {
+            case 1:
+                $("#shapeB").addClass('blue-active');
+                playSound(blueSound);
+                break;
+            case 2:
+                $("#shapeY").addClass('yellow-active');
+                playSound(yellowSound);
+                break;
+            case 3:
+                $("#shapeG").addClass('green-active');
+                playSound(greenSound);
+                break;
+            case 4:
+                $("#shapeO").addClass('orange-active');
+                playSound(orangeSound);
+                break;
+        }
+        removeClass(id)
     }
-    removeClass(id)
 };
-
 function removeClass(id){
-    switch (id) {
-        case 0:
-            $("#shapeB").removeClass('blue-active');
-            break;
-        case 1:
-            $("#shapeY").removeClass('yellow-active');
-            break;
-        case 2:
-            $("#shapeG").removeClass('green-active');
-            break;
-        case 3:
-            $("#shapeO").removeClass('orange-active');
-            break;
-    }
+    setTimeout(function(){
+        switch (id) {
+            case 1:
+                $("#shapeB").removeClass('blue-active');
+                break;
+            case 2:
+                $("#shapeY").removeClass('yellow-active');
+                break;
+            case 3:
+                $("#shapeG").removeClass('green-active');
+                break;
+            case 4:
+                $("#shapeO").removeClass('orange-active');
+                break;
+        }
+    }, 1000)
 };
-
-
-//flashing light  //
-
-function gameCount() {
-    power = false;
-    if (flash == displayCount);
-    clearIntervalId(intervalId);
-   genCompTurn = false;
-    power = true;
-    clearColor(button);
-
-
-    if (genCompTurn) {
-        power = false;
-        setTimeOut(function () {
-            flashColor(simonSeq[flash])
-            flash++;
-        }, 300);
-        clearColor();
-    }
-};
-
-
-
-// user Sounds for button clicks //
-
+// Allow user clicks
 $("#shapeB").click(function () {
-    addpplayerSequence(0);
-});
-
-$("#shapeY").click(function () {
     addpplayerSequence(1);
 });
-
-$("#shapeG").click(function () {
+$("#shapeY").click(function () {
     addpplayerSequence(2);
 });
-
-$("#shapeO").click(function () {
+$("#shapeG").click(function () {
     addpplayerSequence(3);
 });
-
-
-
+$("#shapeO").click(function () {
+    addpplayerSequence(4);
+});
 function addpplayerSequence(id) {
-    if (power && gameStarted) {
+    if (power && gameStarted && flashes === level) {
         playButtonPad(id);
         playerSequence.push(id);
         checkSequence();
     }
 };
-
 function checkSequence() {
     if (playerSequence[playerSequence.length - 1] !== simonSeq[playerSequence.length - 1]) {
-        // Change counter to NO and Replay the game from previous level
+        // Change counter to NO and Replay the game from same level
         displayCountMemory.innerHTML = "NO"
-        play(level)
+        flashes = 0
+        playerSequence = []
+        setTimeout(function(){play(level)}, 2000)
     } else {
-        if (playerSequence.length == level){
+        if (playerSequence.length === level){
             displayCountMemory.innerHTML = "YES"
             level ++
-            play(level)
+            flashes = 0
+            playerSequence = []
+            setTimeout(function(){play(level)}, 2000)
         }
     }
 };
-
-
-// PlaySound //
+// Play a sounnd by Id
 function playSound(soundId){
     if(sound){
         soundId.play();
     }
 };
-
-// function play(sound) {
-
-//     if (noise) {
-//         let audio = document.getElementById("soundB");
-//         audio.play(sound);
-//     }
-//     noise = true;
-//     $("#shapeB").css("background-color", "red");
-// }
-
-// if (noise) {
-//         let audio = document.getElementById("soundY");
-//         audio.play(sound);
-// }
-// noise = true;
-// $("#shapeY").css ("background-color","red");
-
-
-// if (noise) {
-//     let audio = document.getElementById("soundG");
-//     audio.play(sound);
-
-//     noise = true;
-//     $("#shapeG").css ("background-color","LawnGreen"); //#7CFC00
-// }
-// if (noise) {
-//     let audio = document.getElementById("soundO");
-//     audio.play(sound);
-// }
-// noise = true;
-// $("#shapeO").css ("background-color","Indigo"); //#4B0082
-
-
